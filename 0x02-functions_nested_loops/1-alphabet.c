d22c2ce06c8545b0578317ae28c6d2970fee027a
kidanGebre <zemkidzem@gmail.com>
2022-12-13 01:33:40 -0800

    kjfksdh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * main - man nbjhbjh bhjcb
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char lc;
+	for ( lc = 'a'; lc < 'z';lc++)
+	{
+		_putchar(lc);
+	}
+	_putchar('\n');
+}
