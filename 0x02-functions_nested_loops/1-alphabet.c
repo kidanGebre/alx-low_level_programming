589cc5b630cbaf4da3377e7622a15b4e0535bc00
kidanGebre <zemkidzem@gmail.com>
2022-12-13 01:55:20 -0800

    kifds

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
-/*
- * main - man nbjhbjh bhjcb
+/**
+ * print_alphabet - man nbjhbjh bhjcb
  * Return: always 0
  */
-
 void print_alphabet(void)
 {
 	char lc;
-	for ( lc = 'a'; lc < 'z';lc++)
+
+	for (lc = 'a'; lc < 'z'; lc++)
 	{
 		_putchar(lc);
 	}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - man nbjhbjh bhjcb
+ */
+void print_alphabet_x10(void)
+{
+	char lc;
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (lc = 'a'; lc < 'z'; lc++)
+		{
+			_putchar(lc);
+		}
+		_putchar('\n');
+	}
+}
