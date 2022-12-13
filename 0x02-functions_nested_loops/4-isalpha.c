ebbfc36d3d206319d8fd9f910e6a85bf4a2d371e
kidanGebre <zemkidzem@gmail.com>
2022-12-13 02:22:28 -0800

    kif

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,10 @@
 /**
  * _isalpha - maninn jhk jsd
  * Return: 1 if c is a letter, lowercase or uppercase otherwise 0
- * 'c' - is char
+ * @c: is char
  */
 int _isalpha(int c)
 {
-	char c;
-
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
 	else
