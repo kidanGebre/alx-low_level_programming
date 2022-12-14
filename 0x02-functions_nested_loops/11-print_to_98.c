b8a10a2bc53560d0792d5370d68cf021585a9fd9
kidanGebre <zemkidzem@gmail.com>
2022-12-13 23:06:02 -0800

    ki

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,17 +8,13 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		while (n>= 98)
-		{
-			printf(n + ", ");
-			n--;
-		}
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
 	if (n < 98)
 	{
 		while (n <= 98)
-		{
-			printf(n + ", ");
-			n++;
-		}
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
